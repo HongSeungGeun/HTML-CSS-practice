6d26c7b45aaf5750bf2260273e2a228579f62ba4
HongSeungGeun <103030115+HongSeungGeun@users.noreply.github.com>
2023-01-26 18:09:58 +0900

    Delete html+css practice directory

--- html+css practice/main.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('.nav-btn');
-const menu = document.querySelector('.nav-menu');
-const icons = document.querySelector('.nav-icons');
-
-
-btn.addEventListener('click', ()=>{
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-});
